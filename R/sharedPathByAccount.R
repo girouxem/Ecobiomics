e4e1a3386707347afbbfb6cec9b27490d9550a03
minuka819 <minuka819@gmail.com>
2022-03-22 11:38:34 -0400

    1)added print statements to print account and shared path 2)added return statement that will return sharedPath

--- R/sharedPathByAccount.R
+++ R/sharedPathByAccount.R
@@ -3,7 +3,12 @@ setSharedPath <- function(account) {
   # if user = minuka, then sharedPath = 
   if(account != "girouxeml"){
     sharedPath = paste("/isilon/cfia-ottawa-fallowfield/users/girouxeml/", account, "/PIRL_working_directory/", sep = "")
+    print(account)
+    print(sharedPath)
   } else {
     sharedPath = "/isilon/cfia-ottawa-fallowfield/users/girouxeml/PIRL_working_directory"
+    print(account)
+    print(sharedPath)
   }
+  return(sharedPath)
 }
